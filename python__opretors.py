fa58b423f917c85d9751e08878dc76f1f8a0a91d
gauri26-data-analytics <btsarmymrs@gmail.com>
2025-10-09 08:31:23 +0530

    Add files via upload

---
+++ python__opretors.py
@@ -0,0 +1,21 @@
+
+a = 15
+b = 4
+print("Addition:", a + b)  
+print("Subtraction:", a - b) 
+print("Multiplication:", a * b)  
+print("Division:", a / b) 
+print("Floor Division:", a // b)  
+print("Modulus:", a % b) 
+print("Exponentiation:", a ** b)
+
+x=20
+d=50
+e=91
+u=99
+i=15
+k=88
+print("ans=",x+d-e*u/i**k%x//e)
+
+
+
